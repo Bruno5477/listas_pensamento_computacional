195d5d5a50c2ed5c538d743ff65b8eaed913fee2
bruno5477 <brunooliveira.tec@gmail.com>
2024-05-20 00:15:10 -0300

    atividades_13

--- P2/teste2.py
+++ P2/teste2.py
@@ -1,1 +1,7 @@
+lojas = ['Loja A', 'Loja B', 'Loja C', 'Loja D', 'Loja E', 'Loja F', 'Loja G']
+vendas = [10, 100, 50, 80, 15, 40, 120]
+meta = 100
 
+for i, loja in enumerate(lojas):
+  if vendas[i] >= meta:
+    print(f'{loja} -> {vendas[i]}')
